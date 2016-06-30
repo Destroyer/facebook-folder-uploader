c6c76abfe45c14295fb2162cf18e4e52c0beb75f
Destroyer <vlachmir@fit.cvut.cz>
2016-06-30 19:11:03 +0200

    Fixed uploading multiple big files

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-var fs    = require("fs"),
+var fs = require("fs"),
 login = require("facebook-chat-api");
 
 login({email: "mail", password: "pass"}, function callback (err, api) {
-        if(err) return console.error(err);
-        var yourID = 1;
+	if(err) return console.error(err);
+	var yourID = 1;
+	var folder = fs.readdirSync(__dirname + '/temp/');
 
-        var folder = fs.readdirSync(__dirname + '/temp/');
-        folder.forEach(function(value) {
-        	var msg = {
-                        body: value,
-                        attachment: fs.createReadStream(__dirname + '/temp/' + value)
-        	}
-        	api.sendMessage(msg, yourID);
-	});
+	function uploader(i)
+	{
+		if (i < folder.length)
+		{
+			var msg = {
+				body: folder[i],
+				attachment: fs.createReadStream(__dirname + '/temp/' + folder[i])
+			}
+			api.sendMessage(msg, yourID, function(err) {
+				if( err ){console.log('error: '+err);}
+				else {uploader(i+1);}
+			});
+		}
+	}
+	uploader(0);
 });
