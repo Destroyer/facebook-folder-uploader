b242697998557561d25fdf2abdb00f91e53b096f
Destroyer <vlachmir@fit.cvut.cz>
2016-07-05 17:47:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ login = require("facebook-chat-api");
 login({email: "mail", password: "pass"}, function callback (err, api) {
 	if(err) return console.error(err);
 	var yourID = 1;
-	var folder = fs.readdirSync(__dirname + '/temp/');
+	var folder = fs.readdirSync(__dirname + '/temp/parts');
 
-	function uploader(i)
+	function uploader(i) 
 	{
 		if (i < folder.length)
 		{
 			var msg = {
 				body: folder[i],
-				attachment: fs.createReadStream(__dirname + '/temp/' + folder[i])
+				attachment: fs.createReadStream(__dirname + '/temp/parts/' + folder[i])
 			}
 			api.sendMessage(msg, yourID, function(err) {
 				if( err ){console.log('error: '+err);}
@@ -21,4 +21,10 @@ login({email: "mail", password: "pass"}, function callback (err, api) {
 		}
 	}
 	uploader(0);
+	
+	var msg = {body: "\r\n-----------------------------------------" + process.argv[2] + "\r\nCasti: " + folder.length + "; Velikost: " + process.argv[3]};
+    api.sendMessage(msg, yourID);
+
+
 });
+
