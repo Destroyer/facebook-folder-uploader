5bce647ba75e7c513d1df9c3d1451b2f37b28cd7
Destroyer <vlachmir@fit.cvut.cz>
2016-07-19 01:38:28 +0200

    Added logging

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 var fs = require("fs"),
-login = require("facebook-chat-api");
+        login = require("facebook-chat-api"),
+        log = require("npmlog");
+
 
 login({email: "mail", password: "pass"}, function callback (err, api) {
-	if(err) return console.error(err);
-	var yourID = 1;
-	var folder = fs.readdirSync(__dirname + '/temp/parts');
+    if(err) return console.error(err);
+    var yourID = 1;
+    var folder = fs.readdirSync(__dirname + '/temp/parts');
 
-	function uploader(i) 
-	{
-		if (i < folder.length)
-		{
-			var msg = {
-				body: folder[i],
-				attachment: fs.createReadStream(__dirname + '/temp/parts/' + folder[i])
-			}
-			api.sendMessage(msg, yourID, function(err) {
-				if( err ){console.log('error: '+err);}
-				else {uploader(i+1);}
-			});
-		}
-	}
-	uploader(0);
-	
-	var msg = {body: "\r\n-----------------------------------------" + process.argv[2] + "\r\nCasti: " + folder.length + "; Velikost: " + process.argv[3]};
-    api.sendMessage(msg, yourID);
+    function uploader(i)
+        {
+                var j = i+1;
 
+                if (i < folder.length)
+                {
+                        var msg = {
+                                body: folder[i],
+                                attachment: fs.createReadStream(__dirname + '/temp/parts/' + folder[i])
+                        }
+                        log.info("Uploading " + folder[i] + ". " + j + "/" + folder.length);
+                        api.sendMessage(msg, yourID, function(err) {
+                                if( err ){console.log('error: '+err);}
+                                else {uploader(i+1);}
+                        });
+                }
+        }
+        uploader(0);
+
+        var msg = {body: "-----------------------------------------\r\n" + process.argv[2] + "\r\nCasti: " + folder.length + "; Velikost: " + process.argv[3]};
+        api.sendMessage(msg, yourID);
 
 });
-
